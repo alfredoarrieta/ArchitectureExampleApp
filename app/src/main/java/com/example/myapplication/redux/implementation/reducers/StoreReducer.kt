00c1db0bc920aeaf48e36b5c83146616c6fb4f0a
aarrieta <alfredo.arrieta@elektra.com.mx>
2022-08-08 10:21:25 -0500

    * fix code style klint

--- app/src/main/java/com/example/myapplication/redux/implementation/reducers/StoreReducer.kt
+++ app/src/main/java/com/example/myapplication/redux/implementation/reducers/StoreReducer.kt
@@ -5,10 +5,10 @@ import com.example.myapplication.redux.implementation.AppState
 import com.example.myapplication.redux.implementation.actions.ReduxAction
 import com.example.myapplication.redux.implementation.actions.StoreActions
 
-class StoreReducer: BaseReducer() {
+class StoreReducer : BaseReducer() {
 
     override fun reduce(action: ReduxAction, state: AppState): AppState {
-        return when(action){
+        return when (action) {
             is StoreActions.SetStoreProducts -> setStoreProducts(action.products, state)
             else -> state
         }
